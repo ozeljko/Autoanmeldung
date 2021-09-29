2ef0d742dfb7e23bdb4bbecc8f3bf57215c815ff
ozeljko <ozeljko@hotmail.com>
2021-09-29 20:53:22 +0200

    update 0001

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import './Autoanmeldung_styles.css';
+import './Responsive_styles.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
